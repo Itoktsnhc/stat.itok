8ef664e21ae63551d30efb231307deae58fcb51f
itoktsnhc <itoktsnhc@hotmail.com>
2023-01-11 15:06:40 +0800

    try workload?

---
+++ Stat.Itok.Workload/Program.cs
@@ -0,0 +1,17 @@
+namespace Stat.Itok.Workload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
