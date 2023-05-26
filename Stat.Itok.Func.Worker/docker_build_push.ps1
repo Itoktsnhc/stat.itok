2201b895b4d0a0ca83208c6ff756c72eeed6225c
itoktsnhc <itoktsnhc@hotmail.com>
2023-05-26 15:16:54 +0800

    add some build script

---
+++ Stat.Itok.Func.Worker/docker_build_push.ps1
@@ -0,0 +1,9 @@
+param (
+    [string]$image_tag_suffix = (Get-Date -format "yyMMdd_HHmmss")
+)
+$image_tag = "itoktsnhc/stat_itok:$image_tag_suffix"
+$commit_hash = git rev-parse HEAD
+Write-Output "image from COMMIT_HASH:$commit_hash; with tag:$image_tag"
+docker buildx build -f "./Dockerfile" --force-rm -t $image_tag --label "COMMIT_HASH=$commit_hash" ../
+docker push $image_tag
+return $image_tag
